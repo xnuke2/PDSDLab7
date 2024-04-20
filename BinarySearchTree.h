f73042b516f7dbb28519eea5c4ffdfb0daa36d43
xnuke2 <xmnikit@gmail.com>
2024-04-20 23:36:24 +0300

    Добавьте файлы проекта.

---
+++ BinarySearchTree.h
@@ -0,0 +1,102 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using std::vector;
+#include <string>
+using std::string;
+using std::to_string;
+class BinarySearchTree
+{
+private:
+	struct TreeNode {
+
+		int value;
+		TreeNode* left;
+		TreeNode* right;
+		TreeNode(int val)
+		{
+			value = val;
+			left = nullptr;
+			right = nullptr;
+		}
+
+	};
+	TreeNode* head;
+public:
+	BinarySearchTree() {
+		head = NULL;
+	}
+	bool IsEmpty() {
+		return head == NULL;
+	}
+	void Add(int val) {
+		Add(head, val);
+	}
+	void Add(TreeNode* &r, int val)   //функция создания нового дерева
+	{
+		if (!r)
+		{
+			r = new TreeNode(val);
+			r->left = r->right = NULL;
+		}
+		else if (val < r->value)
+			Add(r->left, val);
+		else
+			Add(r->right, val);
+	}
+	BinarySearchTree(int min,int max, int length) {
+		head = NULL;
+		for (int i = 0; i < length; i++) {
+			Add(rand() % (max - min + 1) + min);
+		}
+	}
+	string UpDown() {
+		string tmp ="";
+		tmp = UpDown(head);
+		return tmp;
+	}
+	string UpDown(TreeNode* _head) {
+		string tmp = "";
+		tmp += to_string(_head->value);
+		if (_head->left != NULL)tmp+= " "+UpDown(_head->left);
+		if (_head->right != NULL)tmp += " " + UpDown(_head->right);
+		return tmp;
+	}
+	string LeftRight() {
+		string tmp = "";
+		tmp = LeftRight(head);
+		return tmp;
+	}
+	string LeftRight(TreeNode* _head) {
+		string tmp = "";
+		if (_head->left != NULL)tmp += LeftRight(_head->left)+ " ";
+		tmp += to_string(_head->value);
+		if (_head->right != NULL)tmp += " " + LeftRight(_head->right);
+		return tmp;
+
+	}
+	string DownUp() {
+		string tmp = "";
+		tmp = DownUp(head);
+		return tmp;
+	}
+	string DownUp(TreeNode* _head) {
+		string tmp = "";
+		if (_head->left != NULL)tmp += DownUp(_head->left) + " ";
+		if (_head->right != NULL)tmp += DownUp(_head->right)+" ";
+		tmp += to_string(_head->value);
+		return tmp;
+	}
+	~BinarySearchTree() {
+		Delete(head);
+	}
+	void Delete(TreeNode* &_head) {
+		if (_head->left != NULL)Delete(_head->left);
+		if (_head->right != NULL)Delete(_head->right);
+		delete _head;
+		return;
+		
+
+	}
+};
+
---
+++ PDSDLab7.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+#include "BinarySearchTree.h"
+
+int main()
+{
+    BinarySearchTree* tree1 = new BinarySearchTree();
+    tree1->Add(5);
+    tree1->Add(2);
+    tree1->Add(8);
+    tree1->Add(1);
+    tree1->Add(5);
+    tree1->Add(3);
+    BinarySearchTree* tree = new BinarySearchTree(1, 100, 10);
+    string tmp1 = tree->UpDown();
+    string tmp2 = tree->LeftRight();
+    string tmp3 = tree->DownUp();
+    delete tree;
+}
+
